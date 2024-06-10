c41235a5858916fbe641eb3a8136427da32c8122
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-06-10 16:42:32 +0200

    Create structs.cpp

---
+++ cppCode/structs/structs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+//just experimenting
+
+struct Point3d{
+    int x{0};
+    int y{0};
+    int z{0};
+};
+
+ Point3d& getInput(Point3d& bruh) {
+    std::cout << "Inserisci x y e z:";
+    int x,y,z;
+    std::cin >> x >> y >> z;
+    bruh = {x,y,z};
+    return bruh;
+}
+
+int main() {
+    Point3d bruh{};
+    Point3d damn{getInput(bruh)};
+    std::cout << bruh.x << ',' << bruh.y << ',' << bruh.z << '\n';
+    bruh = {2,3,4};
+    std::cout << bruh.x << ',' << bruh.y << ',' << bruh.z << '\n';
+    return 0;
+}

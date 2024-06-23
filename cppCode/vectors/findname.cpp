2cb03575ade5bb480c06668f44c5050372ec3ed7
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-06-24 00:14:31 +0200

    Create findname.cpp

---
+++ cppCode/vectors/findname.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string_view>
+
+template <typename T>
+bool isInVector(const T& string, const std::vector<std::string_view>& vector) {
+    for (const auto& temp : vector) 
+        if(string == temp) return true;
+    
+    return false;
+}
+
+int main() {
+    std::vector<std::string_view> list {"Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly"};
+    
+    std::string input{};
+    std::cout << "Enter a name: ";
+    std::cin >> input;
+
+    std::cout << '"' << input << '"' << (isInVector(input, list) ? " was" : " wasn't") << " found" << '\n';
+
+}

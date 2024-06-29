c095cd771cf223ac93057c4a23a417a18ea4e48c
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-06-29 13:22:35 +0200

    Create checkSquare.cpp

---
+++ cppCode/arrays/checkSquare.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "failureCin.h"
+
+constexpr int squares[] {0,1,4,9};
+
+bool foundSquare(int input) {
+    for(int i : squares) {
+        if(i==input) return true;
+    }
+    return false;
+}
+
+
+int main() {
+    int input{};
+    while(true) {
+        std::cout << "Enter a number or -1 to quit: ";
+        std::cin >> input;
+
+        if (failureCin()) continue;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        if(input == -1) return 0;
+        else {
+            std::cout << input << (foundSquare(input) ? " is " : " isn't ") << "a perfect square between 0 and 9.\n";
+        }
+    }
+    return 1;
+}

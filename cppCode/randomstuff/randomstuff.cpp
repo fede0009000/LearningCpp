512cb8317c62f09098681d019a7769052792e9fd
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-08-20 14:44:58 +0200

    Update randomstuff.cpp

--- cppCode/randomstuff/randomstuff.cpp
+++ cppCode/randomstuff/randomstuff.cpp
@@ -1,14 +1,5 @@
 #include <array>
 #include <iostream>
+#include <string.h>
 #include <algorithm>
 
-int main()
-{
-    std::array nums{1, 2, 3, 4, 5, 6, 7};
-
-    auto found{std::find(nums.begin(), nums.end(), 5)};
-    if (found == nums.end())
-        std::cout << "Valore non trovato.";
-    else
-        std::cout << "Valore trovato all’indice " << *found;
-}

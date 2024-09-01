637b222ae0147e0a1f37950b0dd58dffdcad4765
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-09-01 17:50:41 +0200

    Create idkkk.cpp

---
+++ cppCode/randomstuff/idkkk.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    char *str = "HmYkBwozJw4WNyAAFyB1VUcqOE1JZjUIBis7ABdmbU1GIjEJU3FjTRg%3D";
+    for (int i{0}; str[i] != '\0'; ++i)
+    {
+        
+    }
+}

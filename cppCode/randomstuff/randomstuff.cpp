d1927de995717955c35d61c16ff405eda75639e1
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-09-04 21:37:49 +0200

    Update randomstuff.cpp

--- cppCode/randomstuff/randomstuff.cpp
+++ cppCode/randomstuff/randomstuff.cpp
@@ -3,3 +3,9 @@
 #include <string.h>
 #include <algorithm>
 
+int main()
+{
+    float fourthirds = 4 / 3;
+    float onethird = 1 / 3;
+    std::cout << fourthirds - onethird; // computer can do math now? crazy
+}

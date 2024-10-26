24df7295186b19044afc2eea30c46a6bff89cfa7
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-10-26 15:03:17 +0200

    Made comments in binarysearch.cpp better

--- cppCode/algorithms/binarysearch.cpp
+++ cppCode/algorithms/binarysearch.cpp
@@ -4,10 +4,10 @@
 #include <cassert>
 
 // array is the array to search over.
-// target is the value we're trying to determine exists or not.
+// target is the value we're looking for.
 // min is the index of the lower bounds of the array we're searching.
 // max is the index of the upper bounds of the array we're searching.
-// binarySearch() should return the index of the target element if the target is found, -1 otherwise
+// binarySearch() -> -1 if not found or the index of the target if found
 int binarySearch(const int* array, int target, int min, int max)
 {
     assert(array);

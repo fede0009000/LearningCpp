13f2f6320e4204114a9d1712f21ad1f25647b420
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-10-26 14:56:16 +0200

    Made hilo.cpp more readable and customizable

--- cppCode/hiLo/hiLo.cpp
+++ cppCode/hiLo/hiLo.cpp
@@ -1,94 +1,89 @@
 #include <iostream>
 #include "Random.h"
 #include <limits>
+#include "failureCin.h"
 
-void ignoreLine() // for error handling
+namespace Settings
 {
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    int max_num{100};   // I'm thinking of a number between 1 and max_num
+    int max_guesses{7}; // Maximum number of guesses before the player loses
 }
-
-void ignoreLine();
 
 void hiLo()
 {
-    int guess{0};
-    int num{0};
-    for (int tryn{1}; tryn <= 7; ++tryn)
+    // Settings::max_num is 100 by default.
+    const int num{Random::get(1, Settings::max_num)};
+
+    // Description
+    std::cout << "Let's play a game. I'm thinking of a number between 1 and "
+              << Settings::max_num // Settings::max_num is 100 by default
+              << ". You have "
+              << Settings::max_guesses << " tries to guess what it is. " << '\n'; // Settings::max_guesses is 7 by default
+
+    // Actual game starts here
+    int guess{0}; // Used to store the player's guess
+
+    // guess_number indicates the number of the guess (eg. Guess #1, Guess#2,...)
+    for (int guess_number{1}; guess_number <= Settings::max_guesses; ++guess_number)
     {
+        std::cout << "Guess #" << guess_number << ": ";
 
-        num = Random::get(1, 100);
+        // Get input, sanitize cin buffer to avoid issues later.
+        std::cin >> guess;
+        failureCin();
 
-        std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. You have 7 tries to guess what it is. " << '\n';
-
-        while (true)
+        // WIN
+        if (guess == num)
         {
-            if (tryn > 7)
-            {
-                std::cout << "Sorry, you lose. The correct number was " << num << '\n';
-                return;
-            }
+            std::cout << "Correct! You win!\n";
+            return; // return to caller (main)
+        }
 
-            std::cout << "Guess #" << tryn << ": " << '\n';
+        // If this is the last guess, the player has lost already
+        if (guess_number == Settings::max_guesses)
+            break;
 
-            std::cin >> guess;
-
-            if (std::cin.peek() != '\n')
-            {
-                std::cin.clear();
-                ignoreLine();
-                continue;
-            }
-
-            if (guess == num)
-            {
-                std::cout << "Correct! You win!\n";
-                return;
-            }
-            else if (guess >= 1 && guess <= 100)
-            {
-                std::cout << "Your guess is too " << (guess > num ? "high" : "low") << '\n';
-                ++tryn;
-            }
-
-            std::cin.clear();
-            ignoreLine();
+        // INCORRECT
+        else if (guess >= 1 && guess <= 100) // If you're at the last guess, no need to give more hints.
+        {
+            std::cout << "Your guess is too " << (guess > num ? "high.\n" : "low.\n");
 
             continue;
         }
+
+        // If control reaches here, the guess was invalid
+        --guess_number;
     }
+
+    std::cout << "Sorry, you lose. The correct number was " << num << '\n';
+    return;
 }
 
-bool playAgain()
+bool playAgain() // Once an hiLo game is completed, the user is prompted to play the game again.
 {
     char playAgain{'n'};
     while (true)
     {
-        std::cout << "Would you like to play again (y/n)? " << '\n';
+        // Prompt user
+        std::cout << "\nWould you like to play again? [y/n]\n";
+
+        // Get input, sanitize cin buffer to avoid issues later.
         std::cin >> playAgain;
+        failureCin();
 
-        if (std::cin.peek() != '\n')
-        {
-            std::cin.clear();
-            ignoreLine();
-            continue;
-        }
-        std::cin.clear();
-        ignoreLine();
-
-        switch (playAgain)
-        {
-        case 'y':
+        // Return to caller (main) whether the user wants to keep playing (1) or not (0)
+        if (playAgain == 'y' || playAgain == 'Y')
             return 1;
-        case 'n':
-            return 0;
-        }
+
+        // By default, stop playing
+        return 0;
     }
     return 0;
 }
 
 int main()
 {
-    while (true)
+    while (true) // This loop runs the hiLo function until the user chooses to stop playing.
     {
         hiLo();
         if (playAgain())

162e6f6eb8fef065281332764d6213c3990f3ff3
FedeGnomed <76182933+fede0009000@users.noreply.github.com>
2024-12-18 14:24:18 +0100

    Update space_remover.cpp

--- cppCode/tools_ig/space_remover.cpp
+++ cppCode/tools_ig/space_remover.cpp
@@ -6,9 +6,12 @@ void remove_spaces(std::string& text)
 {
     std::size_t len{text.length()};
     int space_counter{0};
+    char currC{};
     for (std::size_t i{0}; i < len; ++i)
     {
-        if(std::isspace(text[i]))
+        currC = text[i];
+
+        if(std::isspace(currC))
             space_counter += 1;
         else
             space_counter = 0;
@@ -18,6 +21,14 @@ void remove_spaces(std::string& text)
             text.erase(text.begin() + static_cast<long long>(i));
             space_counter = 0;
         }
+
+        // Remove dashes for newlines
+        if (currC == '-')
+        {
+            text.erase(text.begin() + static_cast<long long>(i));
+            text.erase(text.begin() + static_cast<long long>(i));
+            continue;
+        }
     }
 }
 
@@ -26,15 +37,28 @@ int main()
     std::string text{};
     std::string input{};
 
-    std::cout << "Please enter the text with useless spaces you want to remove. It must end with __STOP__.\n";
-    do
+    std::cout << "Please enter the text with useless spaces you want to remove. It must end with __STOP__ or an empty line.\n";
+    while(true)
     {
-        text.append(input);
         std::getline(std::cin, input);
-    } while (input != "__STOP__");
+
+        if (input == "__STOP__" || input.empty())
+            break;
+        
+        if (std::isspace(*(input.end()-1)))
+        {
+            input.erase(input.end() - 1);
+            text.append(input);
+            continue;
+        }
+            
+        text.append(' ' + input);
+    } 
 
     remove_spaces(text);
 
     std::cout << "\n--------------------------------------------\n";
     std::cout << "Here is your text without superflous spaces:\n\n\n" << text;
+
+    return 0;
 }
